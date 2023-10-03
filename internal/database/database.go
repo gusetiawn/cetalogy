fb428a9eceb4ea9f6d52101f86b3e1d3d3b60ad3
gusetiawn <gusetiawn@gmail.com>
2023-10-03 22:15:44 +0700

    commit

--- internal/database/database.go
+++ internal/database/database.go
@@ -12,7 +12,7 @@ var FirestoreClient *firestore.Client
 
 func InitializeFirestore() {
 	ctx := context.Background()
-	opt := option.WithCredentialsFile("credentials.json")
+	opt := option.WithCredentialsFile("/Users/gusetiawn/Development/cetalogy/gigs-dev-9ab8a-firebase-adminsdk-5h99k-063afcfba8.json")
 	client, err := firestore.NewClient(ctx, "gigs-dev-9ab8a", opt)
 	if err != nil {
 		log.Fatalf("Error initializing Firestore: %v", err)
